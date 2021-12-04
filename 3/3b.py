276dde446758f1b611aa19319d91a25ae832380c
Kalenghil <37372132+Kalenghil@users.noreply.github.com>
2021-12-04 15:55:26 +0300

    Updated solution for 3b

--- 3/3b.py
+++ 3/3b.py
@@ -10,10 +10,9 @@ def amount_of_nums_by_digit(nums, len_of_num):
 
 
 file_name = r'3.txt'
-len_of_num = 12
 with open(file_name, 'r') as f:
-    nums = f.readlines()
-
+    nums = list(map(lambda x: x.strip(), f.readlines()))
+len_of_num = len(nums[0])
 a = amount_of_nums_by_digit(nums, len_of_num)
 
 print(a)
@@ -22,25 +21,25 @@ print(a)
 oxygen_generator_rating = nums
 for i in range(len_of_num):
     a = amount_of_nums_by_digit(oxygen_generator_rating, len_of_num)
-    if len(oxygen_generator_rating) == 2:
+    if len(oxygen_generator_rating) == 1:
         break
-    if a[i] > 0:
+    if a[i] >= 0:
         oxygen_generator_rating = list(filter(lambda x: x[i] == '1', oxygen_generator_rating))
     else:
         oxygen_generator_rating = list(filter(lambda x: x[i] == '0', oxygen_generator_rating))
     print(oxygen_generator_rating, a, i)
-oxygen_generator_rating = int(oxygen_generator_rating[1], 2)
+oxygen_generator_rating = int(oxygen_generator_rating[0], 2)
 
 CO2_scrubber_rating = nums
 for i in range(len_of_num):
     a = amount_of_nums_by_digit(CO2_scrubber_rating, len_of_num)
-    if len(CO2_scrubber_rating) == 2:
+    if len(CO2_scrubber_rating) == 1:
         break
-    if a[i] > 0:
+    if a[i] >= 0:
         CO2_scrubber_rating = list(filter(lambda x: x[i] == '0', CO2_scrubber_rating))
     else:
         CO2_scrubber_rating = list(filter(lambda x: x[i] == '1', CO2_scrubber_rating))
     print(CO2_scrubber_rating, a, i)
-CO2_scrubber_rating = int(CO2_scrubber_rating[1], 2)
+CO2_scrubber_rating = int(CO2_scrubber_rating[0], 2)
 
 print(oxygen_generator_rating, CO2_scrubber_rating, oxygen_generator_rating * CO2_scrubber_rating)
